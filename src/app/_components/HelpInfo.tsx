4a54bbb92e2bf7d847099bbda574228b684fd721
Alexander Ott <66271487+AlexanderHott@users.noreply.github.com>
2023-12-26 17:57:06 -0800

    add help text and links

--- src/app/_components/HelpInfo.tsx
+++ src/app/_components/HelpInfo.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ExternalLink, HelpCircle } from "lucide-react";
+import { Bug, ExternalLink, Github, HelpCircle } from "lucide-react";
 import Link from "next/link";
 import { Button } from "~/components/ui/button";
 import {
@@ -18,17 +18,17 @@ export function HelpInfo() {
           <HelpCircle />
         </Button>
       </SheetTrigger>
-      <SheetContent className="flex grow-0 flex-col">
+      <SheetContent className="flex flex-col justify-between">
         <SheetHeader className="flex flex-col">
           <SheetTitle>Help</SheetTitle>
-          <SheetDescription>
+          <SheetDescription className="text-lg">
             The goal of this game is not to earn money, it's to have all the
-            cats. Money is just a tool to achieve that. You gain money by hiring
-            more Baristas, buying more menu items, and more importantly,
+            cats :3. Money is just a tool to achieve that. You gain money by hiring
+            more Baristas, buying more menu items, and most importantly,
             attracting more cats. More cats show up when you have more toys.
           </SheetDescription>
         </SheetHeader>
-        <div>
+        <div className="flex gap-2">
           <Link
             href="https://github.com/AlexanderHott/idle-cat-cafe/discussions"
             target="_blank"
@@ -36,6 +36,23 @@ export function HelpInfo() {
             <Button className="items-center gap-2">
               Feedback
               <ExternalLink className="h-4 w-4"/>
+            </Button>
+          </Link>
+          <Link
+            href="https://github.com/AlexanderHott/idle-cat-cafe/issues/new/choose"
+            target="_blank"
+          >
+            <Button className="items-center gap-2">
+              Report a bug
+              <Bug className="h-4 w-4"/>
+            </Button>
+          </Link>
+          <Link
+            href="https://github.com/AlexanderHott/idle-cat-cafe/"
+            target="_blank"
+          >
+            <Button variant={"secondary"} size="icon" className="items-center gap-2">
+              <Github className="h-4 w-4"/>
             </Button>
           </Link>
         </div>
