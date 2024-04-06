a0acfe089f4331295234f3f9c9a4963d288c9941
Alexander Ott <66271487+AlexanderHott@users.noreply.github.com>
2024-04-06 15:48:37 -0400

    add pwa code

--- src/app/3/Game.tsx
+++ src/app/3/Game.tsx
@@ -3,21 +3,36 @@ import { Sprite, Stage } from "@pixi/react";
 import { useCallback, useEffect, useMemo, useState } from "react";
 
 export default function Game() {
-  const [height, setHeight] = useState(window.innerHeight);
-  const width = useMemo(() => (height * 1692) / 615, [height]);
+  // const [height, setHeight] = useState(window.innerHeight);
+  // const width = useMemo(() => (height * 1692) / 615, [height]);
+  //
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
 
   const onResize = useCallback(() => {
-    setHeight(window.innerHeight);
-  }, [setHeight]);
-
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+    // setHeight(window.innerHeight);
+  }, []);
+  //
   useEffect(() => {
     addEventListener("resize", onResize);
     return () => removeEventListener("resize", onResize);
   }, [onResize]);
 
   return (
-    <Stage options={{ background: 0xff3860 }} height={height} width={width}>
-      <Sprite image="/assets/background.webp" height={height} width={width} />
+    <Stage
+      className="game"
+      options={{ background: 0xff3860 }}
+      // height={height}
+      // width={width}
+    >
+      <Sprite image="/assets/background.webp" 
+        // height={height} 
+        // width={width} 
+      />
     </Stage>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import {
   Pixelify_Sans,
 } from "next/font/google";
 import { cookies } from "next/headers";
+import type { Metadata, Viewport } from "next";
 
 import { TRPCReactProvider } from "~/trpc/react";
 
@@ -18,10 +19,49 @@ const pixelifySans = Pixelify_Sans({
   variable: "--font-sans",
 });
 
-export const metadata = {
-  title: "Idle Cat Cafe",
-  description: "The best cat cafe game ever",
+const APP_NAME = "Idle Cat Cafe";
+const APP_DEFAULT_TITLE = "Idle Cat Cafe";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = "The best cat cafe game ever";
+
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
   icons: [{ rel: "icon", url: "/icons/favicon.ico" }], // TODO: add the rest of the icons
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({
@@ -31,6 +71,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head />
       <body
         className={`font-sans ${pixelifySans.variable} ${pixelifySans.className} dark`}
       >
