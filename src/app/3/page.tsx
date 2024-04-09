de91d1acc359ada1568e9b939afecd51d4e77df8
Alexander Ott <66271487+AlexanderHott@users.noreply.github.com>
2024-04-08 20:56:11 -0400

    fix scrolling

--- src/app/3/page.tsx
+++ src/app/3/page.tsx
@@ -3,9 +3,5 @@ import dynamic from "next/dynamic";
 const Game = dynamic(() => import("./Game"), { ssr: false });
 
 export default function Page() {
-  return (
-    <div className="overflow-x-scroll">
-      <Game />
-    </div>
-  );
+  return <Game />;
 }
